6d7d91d96178515c62f168b7fd2e778284dc221f
Tilak <tilaksk2003@gmail.com>
2023-07-15 17:31:39 +0530

    -

---
+++ data structures/daa/horspool.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+#define MAX 500
+int t[MAX];
+void shifttable(char p[])
+{
+ int i,j,m;
+ m=strlen(p);
+ for(i=0;i<MAX;i++)
+  t[i]=m;
+ for(j=0;j<m-1;j++)
+  t[p[j]]=m-1-j;
+}
+int horspool(char src[],char p[])
+{
+ int i,j,k,m,n;
+ n=strlen(src);
+ m=strlen(p);
+ printf("Length of text:%d\n",n);
+ printf("Length of pattern:%d\n",m);
+ i=m-1;
+ while(i<n)
+ {
+  k=0;
+  while((k<m)&&(p[m-1-k]==src[i-k]))
+   k++;
+  if(k==m)
+   return(i-m+1);
+  else
+   i+=t[src[i]];
+ }
+ return -1;
+}
+void main()
+{
+ char src[100],p[100];
+ int pos;
+ 
+ printf("Enter the text:");
+ gets(src);
+ printf("Enter the pattern to be searched:");
+ gets(p);
+ shifttable(p);
+ pos=horspool(src,p);
+ if(pos>=0)
+  printf("Pattern found starting from position %d\n",pos+1);
+ else
+  printf("Pattern not found in the text\n");
+}
---
+++ data structures/daa/knapsack.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int max(int x, int y)
+{return ((x > y) ? x : y);}
+
+int knap(int n, int w[10], int val[10], int m, int v[10][10])
+{
+    int i, j;
+    for (i = 0; i <= n; i++)
+        for (j = 0; j <= m; j++)
+        {
+            if (i == 0 || j == 0)
+                v[i][j] = 0;
+            else if (j < w[i])
+                v[i][j] = v[i - 1][j];
+            else
+                v[i][j] = max(v[i - 1][j], val[i] + v[i - 1][j - w[i]]);
+    }
+    printf("\n The table for solving knapsack problem using dynamic programming is:\n");
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= m; j++)
+        {
+            printf("%d\t", v[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void main()
+{
+    int v[10][10], n, i, j, w[10], val[10], m, res;
+    printf("Enter the number of items:");
+    scanf("%d", &n);
+    printf("Enter the weights of %d items:", n);
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &w[i]);
+    }
+    printf("Enter the value of %d items:", n);
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &val[i]);
+    }
+    printf("Enter the capacity of the basket:");
+    scanf("%d", &m);
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= m; j++)
+        {
+            v[i][j] = 0;
+        }
+    }
+    res = knap(n, w, val, m, v);
+    printf("Optimal solution for the knapsack problem is %d\n", v[n][m]);
+}
